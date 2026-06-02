d89358fffc44374c402c2e3faa76ed8ef6f26e0d
Your Name <youremail@email.com>
2026-06-02 21:57:43 +0100

    Add app file

---
+++ app.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk, font
+import requests
+from bs4 import BeautifulSoup
+import threading
+
+# CONSTANTS - the things that will not change throughout the code
+
+# the following are the URLs ive used to scrape
+
+URL {}
