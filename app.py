729ecd31c100a747aa6be06c982d00738fa0bc48
Your Name <youremail@email.com>
2026-06-03 21:07:15 +0100

    Update app

--- app.py
+++ app.py
@@ -8,4 +8,8 @@ import threading
 
 # the following are the URLs ive used to scrape
 
-URL {}
+URL = {
+    "movies": "https://www.imdb.com/search/title/?title_type=feature&sort=num_votes,desc&count=50",
+    "tvshows": "https://www.imdb.com/search/title/?title_type=tv_series&sort=num_votes,desc&count=50",
+}
+
